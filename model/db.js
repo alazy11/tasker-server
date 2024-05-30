80d69bc4c45cad79c82117bff2ebd47a54e9dc6f
alazy alhimeari <alazyalhimeari11@gmail.com>
2024-05-31 00:33:45 +0300

    last mod

--- model/db.js
+++ model/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const pool = mysql.createPool({
    connectionLimit :10,
-   host            :'localhost',
-   user            : 'root',
-   password        : '',
-   database        :'tasker'
+   host            :process.env.DB_HOST,
+   user            : process.env.DB_USER,
+   password        : process.env.DB_PASSWORD,
+   database        : process.env.DB_NAME
 });
 
 // const connection = mysql.createConnection({
