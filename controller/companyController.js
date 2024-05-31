5a36cdcc8891837097a15287d5868ada29ed29de
alazy alhimeari <alazyalhimeari11@gmail.com>
2024-06-01 01:05:17 +0300

    getting

--- controller/companyController.js
+++ controller/companyController.js
@@ -218,7 +218,7 @@ const login = (req, res, next) => {
                         secure: true,
                         httpOnly: true,
                         sameSite: "none",
-                        // domain: ".tasker-client-steel.vercel.app",
+                        domain: ".tasker-client-steel.vercel.app",
                         path: "/en/company",
                         maxAge: 86400000,
                      });
@@ -229,7 +229,8 @@ const login = (req, res, next) => {
                      // secure: true,
                      // httpOnly: true,
                      sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
-                     domain: process.env.DOMAIN,
+                     // domain: process.env.DOMAIN,
+                     domain: ".tasker-client-steel.vercel.app",
                      path: "/en/company",
                      maxAge: 86400000,
                   });
