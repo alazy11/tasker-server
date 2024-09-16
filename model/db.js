3fca7d1e79c8fa974922e4bf2c2dad61caa33b6f
alazy11 <alazyalhimeari11@gmail.com>
2024-09-16 18:13:38 +0300

    last update

--- model/db.js
+++ model/db.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-   connectionLimit :10,
    host            :process.env.DB_HOST,
    user            : process.env.DB_USER,
    password        : process.env.DB_PASSWORD,
